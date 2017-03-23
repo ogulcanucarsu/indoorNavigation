3439ba3741acdf4cf19edfbc3081b14f7a718df4
Zafer Bozkurt <zbzkurt@gmail.com>
2017-03-23 23:04:12 +0300

    konum
    konum

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('anaSayfaCtrl', function($scope, $cordovaBarcodeScanner) 
 {
+    
     var basarili=true;
     $scope.barcode=function()
     {
@@ -33,41 +34,138 @@ angular.module('starter.controllers', [])
     }
 })
 
-.controller('dropDownCtrl', function($scope,Chats) 
+.controller('dropDownCtrl', function($scope,$stateParams,Chats) 
 {
-  $scope.barcodeData=barcodeSonuc;
+//   $scope.barcodeData=barcodeSonuc;
 
   //veriTabani(barcodeSonuc);
-  if(barcodeSonuc==19874)
-  {
-    $scope.bulundugunYer='Bilgisayar Mühendisliği Giriş Katındasınız';
-  }
-
+//   if(barcodeSonuc==19874)
+//   {
+//     $scope.bulundugunYer='Bilgisayar Mühendisliği Giriş Katındasınız';
+//   }
+    // Chats.push();
     $scope.liste = Chats.all();
 
 
+
+
+   //AÇ $scope.bulundugunYer=Chats.get(barcodeSonuc).s_name;
+    
 })
 
 .controller('haritaCtrl', function($scope, $stateParams, Chats) 
 {
+    var pusulaDegeri=0;
+    var ilkDeger=0;
+    var ikinciDeger=0;
+    // var doksanDegeri=90;
+    function onSuccess(heading) {
+
+        pusulaDegeri=heading.magneticHeading;
+        pusulaDegeri=pusulaDegeri.toFixed(0);
+        pusulaDegeri=parseFloat(pusulaDegeri);
+
+        console.log(pusulaDegeri);
+
+        pusulaListe.push(pusulaDegeri); 
+    };
+
+    function onError(compassError) {
+        alert('Compass error: ' + compassError.code);
+    };
+
+    var options = {
+        frequency: 500
+    }; 
+
+    navigator.compass.watchHeading(onSuccess, onError, options);
+    
+
     $scope.secilenYer = Chats.get($stateParams.haritaId);
+    $scope.harita='img/katPlaniGuncel2.png';
+    $scope.buradasiniz='img/buradasiniz2.png';
+
+    
+    document.getElementById("buradasinID").style["left"] = solHiza+"px";
+    document.getElementById("buradasinID").style["top"] = topHiza+"px";  
+    
+
+    
+
+    var successHandler = function (pedometerData) {
+        ilkDeger=ikinciDeger;
+        ikinciDeger=pedometerData.numberOfSteps;
+        var fark=(ikinciDeger-ilkDeger);
+
+        hareketEt(fark);
+
+        $scope.$apply(function () 
+            {
+                $scope.adim="Adim: "+ pedometerData.numberOfSteps;
+                $scope.fark="Fark: "+ fark;
+            })
+         
+    };
+    
+    var onError = function(error){
+            console.log("Error in tracking pedometer data: ", error);
+        };
+
+    pedometer.startPedometerUpdates(successHandler, onError);
+    
 })
 
 .controller('hakkimizdaCtrl', function($scope) 
 {
-  
+
+//ADIMSAYAR
+    // var successHandler = function (pedometerData) {
+    //     console.log(pedometerData.numberOfSteps);
+    // };
+    
+    // var onError = function(error){
+    //         console.log("Error in tracking pedometer data: ", error);
+    //     };
+
+    // pedometer.startPedometerUpdates(successHandler, onError);
+
+
+
+//PUSULA
+// document.addEventListener("deviceready", onDeviceReady, false);
+// function onDeviceReady() {
+
+//     function onSuccess(heading) {
+//         console.log(heading.magneticHeading);
+//     };
+
+//     function onError(compassError) {
+//         alert('Compass error: ' + compassError.code);
+//     };
+
+//     var options = {
+//         frequency: 100
+//     }; 
+
+//     navigator.compass.watchHeading(onSuccess, onError, options);
+// }
+var arr=[];
+var gelenListe=[];
+var i=0;
+$scope.listeEkle=function()
+{
+    arr.push(i);
+    i++;
+}
+
+$scope.listeYazdir=function()
+{
+    gelenListe=arr.slice(Math.max(arr.length - 5, 0));
+}
+
 })
 
 
 
-// .controller('ChatDetailCtrl', function($scope) 
-// {
-  
-// })
-
-// .controller('AccountCtrl', function($scope) 
-// {
-  
-// })
 
 ;
--- www/js/services.js
+++ www/js/services.js
@@ -14,7 +14,8 @@ angular.module('starter.services', [])
       {s_id:6, s_name:'Kimya Mühendisligi 509'},
       {s_id:7, s_name:'Kimya Mühendisligi 510'},
       {s_id:8, s_name:'Endüstri Mühendisligi Lab 350'},
-      {s_id:9, s_name:'Kimya Mühendisligi Billab 150'}
+      {s_id:9, s_name:'Kimya Mühendisligi Billab 150'},
+      {s_id:19874, s_name:'OGULCAN'}      
   ];
 
   return {
@@ -29,5 +30,142 @@ angular.module('starter.services', [])
       }
       return null;
     }
+    // push: function()
+    // {
+    //     for(var i=0;i<3;i++)
+    //     {
+    //       liste.push({s_id:9, s_name:'Eklendi'});
+    //     }
+    // }
   };
 });
+
+var pusulaListe =[];
+var adimPusulaListe=[];
+
+var solHiza=150;
+var topHiza=230;
+
+function hareketEt(adim)
+{
+      adimPusulaListe=pusulaListe.slice(Math.max(pusulaListe.length - adim, 0));
+
+        for(var i=0;i<adim;i++)
+        {
+            pusulaDegeri=adimPusulaListe[i];
+            if(pusulaDegeri>=22.5 && pusulaDegeri<=67.5)          
+            {
+                solaGit();
+                yukariGit();
+            }
+            else if(pusulaDegeri>=67.5 && pusulaDegeri<=112.5)
+            {
+                yukariGit();
+            }
+            else if(pusulaDegeri>=112.5 && pusulaDegeri<=157.5)
+            {
+                sagaGit();
+                yukariGit();
+            }
+            else if(pusulaDegeri>=157.5 && pusulaDegeri<=202.5)
+            {
+                sagaGit();
+            }
+            else if(pusulaDegeri>=202.5 && pusulaDegeri<=247.5)
+            {
+                sagaGit();
+                asagiGit();
+            }
+            else if(pusulaDegeri>=247.5 && pusulaDegeri<=292.5)
+            {
+                asagiGit();
+            }
+            else if(pusulaDegeri>=292.5 && pusulaDegeri<=337.5)
+            {
+                solaGit();
+                asagiGit();
+            }
+            else if(pusulaDegeri>=337.5 && pusulaDegeri<=360)
+            {
+                solaGit();
+            }
+            else if(pusulaDegeri>=0 && pusulaDegeri<=22.5)
+            {
+                solaGitf();
+            }
+        }
+}
+function solaGit()
+    {
+        var img = new Image();
+        img.src = 'img/buradasiniz2.png';
+        img.style["left"] = solHiza+"px";
+        img.style["top"] = topHiza+ "px";    
+        img.style["position"] = "absolute";  
+
+        var div = document.getElementById('deneme');
+
+        img.onload = function() {
+            div.appendChild(img);
+        };
+
+        solHiza=solHiza-5;
+        document.getElementById("buradasinID").style["left"] = solHiza+"px";
+    } 
+function sagaGit()
+    {
+        var img = new Image();
+        img.src = 'img/buradasiniz2.png';
+        img.style["left"] = solHiza+"px";
+        img.style["top"] = topHiza+ "px";    
+        img.style["position"] = "absolute";  
+
+        var div = document.getElementById('deneme');
+
+        img.onload = function() {
+            div.appendChild(img);
+        };
+
+        solHiza=solHiza+5;
+        // topHiza=topHiza+1;
+        document.getElementById("buradasinID").style["left"] = solHiza+"px";
+        // document.getElementById("buradasinID").style["top"] = topHiza+"px";  
+    }
+function asagiGit()
+    {
+        var img = new Image();
+        img.src = 'img/buradasiniz2.png';
+        img.style["left"] = solHiza+"px";
+        img.style["top"] = topHiza+ "px";    
+        img.style["position"] = "absolute";  
+
+        var div = document.getElementById('deneme');
+
+        img.onload = function() {
+            div.appendChild(img);
+        };
+
+        // solHiza=solHiza+1;
+        topHiza=topHiza+5;
+        // document.getElementById("buradasinID").style["left"] = solHiza+"px";
+        document.getElementById("buradasinID").style["top"] = topHiza+"px";  
+    }
+function yukariGit()
+    {
+        var img = new Image();
+        img.src = 'img/buradasiniz2.png';
+        img.style["left"] = solHiza+"px";
+        img.style["top"] = topHiza+ "px";    
+        img.style["position"] = "absolute";  
+
+        var div = document.getElementById('deneme');
+
+        img.onload = function() {
+            div.appendChild(img);
+        };
+
+        // solHiza=solHiza+1;
+        topHiza=topHiza-5;
+        // document.getElementById("buradasinID").style["left"] = solHiza+"px";
+        document.getElementById("buradasinID").style["top"] = topHiza+"px";  
+    }
